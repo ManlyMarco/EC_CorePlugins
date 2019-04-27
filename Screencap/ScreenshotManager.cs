515baa58f1938ea2be8ab4007442521b044c8faf
ManlyMarco <dummyaccx@gmail.com>
2019-04-27 18:06:29 +0200

    Initial project setup

---
+++ Screencap/ScreenshotManager.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using ECCorePlugins;
+
+namespace Screencap
+{
+    [BepInPlugin(GUID, "Screenshot Manager", Metadata.PluginsVersion)]
+    public class ScreenshotManager : BaseUnityPlugin
+    {
+        public const string GUID = "com.bepis.bepinex.screenshotmanager";
+
+        private void Awake()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Start()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Update()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnGUI()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Shared/Metadata.cs
@@ -0,0 +1,12 @@
+﻿namespace ECCorePlugins
+{
+    internal static class Metadata
+    {
+        /// <summary>
+        /// Version number used be all plugins. Must be convertible to <see cref="System.Version"/>
+        /// Use ReleaseVersion.HotfixVersion format
+        /// </summary>
+        public const string PluginsVersion = "0.1";
+        public const string CompanyName = "EC_CorePlugins";
+    }
+}
