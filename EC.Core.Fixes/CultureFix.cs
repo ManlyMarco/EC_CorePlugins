43e589b80ee95add4e8b6156239a95499499cb8a
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-04-27 22:00:27 -0500

    Rename project

--- EC.Core.Fixes/CultureFix.cs
+++ EC.Core.Fixes/CultureFix.cs
@@ -2,12 +2,12 @@
 using BepInEx;
 using EC.Core.Internal;
 
-namespace EC.Core.Fixes
+namespace EC.Core.Fixes.CultureFix
 {
-    [BepInPlugin(GUID, "Core Fixes", Version)]
-    public class CoreFixes : BaseUnityPlugin
+    [BepInPlugin(GUID, "Culture Fix", Version)]
+    public class CultureFix : BaseUnityPlugin
     {
-        public const string GUID = "EC.Core.Fixes";
+        public const string GUID = "EC.Core.Fixes.CultureFix";
         public const string Version = Metadata.PluginsVersion;
 
         private void Awake()
