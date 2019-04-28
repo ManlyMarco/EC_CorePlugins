9df8d403a2d8436100104aa113815288c7738501
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-04-27 19:28:44 -0500

    Added SliderUnlocker

---
+++ EC.Core.SliderUnlocker/SliderUnlocker.Hooks.cs
@@ -0,0 +1,155 @@
+﻿using ChaCustom;
+using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using UnityEngine;
+using BepInEx.Harmony;
+
+namespace EC.Core.SliderUnlocker
+{
+    public static class Hooks
+    {
+        public static void InstallHooks()
+        {
+            BepInEx.Harmony.HarmonyWrapper.PatchAll(typeof(Hooks));
+        }
+
+        private static FieldInfo akf_dictInfo = typeof(AnimationKeyInfo).GetField("dictInfo", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        [HarmonyPostfix, HarmonyPatch(typeof(CustomBase), "ConvertTextFromRate")]
+        public static void ConvertTextFromRateHook(ref string __result, int min, int max, float value)
+        {
+            if (min == 0 && max == 100)
+                __result = Math.Round(100 * value).ToString(CultureInfo.InvariantCulture);
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(CustomBase), "ConvertRateFromText")]
+        public static void ConvertRateFromTextHook(ref float __result, int min, int max, string buf)
+        {
+            if (min == 0 && max == 100)
+            {
+                if (buf == null || buf == "")
+                {
+                    __result = 0f;
+                }
+                else
+                {
+                    if (!float.TryParse(buf, out float val))
+                    {
+                        __result = 0f;
+                    }
+                    else
+                    {
+                        __result = val / 100;
+                    }
+                }
+            }
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(Mathf), "Clamp", new[] { typeof(float), typeof(float), typeof(float) })]
+        public static void MathfClampHook(ref float __result, float value, float min, float max)
+        {
+            if (min == 0f && max == 100f)
+                __result = value;
+        }
+        [ParameterByRef(new[] { 2 })]
+        [HarmonyPrefix, HarmonyPatch(typeof(AnimationKeyInfo), "GetInfo", new[] { typeof(string), typeof(float), typeof(Vector3), typeof(byte) })]
+        public static void GetInfoSingularPrefix(ref float __state, ref float rate)
+        {
+            __state = rate;
+
+            if (rate > 1)
+                rate = 1f;
+
+
+            if (rate < 0)
+                rate = 0f;
+        }
+        [ParameterByRef(new[] { 2 })]
+        [HarmonyPostfix, HarmonyPatch(typeof(AnimationKeyInfo), "GetInfo", new[] { typeof(string), typeof(float), typeof(Vector3), typeof(byte) })]
+        public static void GetInfoSingularPostfix(AnimationKeyInfo __instance, bool __result, float __state, string name, ref float rate, ref Vector3 value, byte type)
+        {
+            if (!__result)
+                return;
+
+            rate = __state;
+
+            if (rate < 0f || rate > 1f)
+            {
+                var dictInfo = (Dictionary<string, List<AnimationKeyInfo.AnmKeyInfo>>)akf_dictInfo.GetValue(__instance);
+
+                List<AnimationKeyInfo.AnmKeyInfo> list = dictInfo[name];
+
+                switch (type)
+                {
+                    case 0:
+                        value = SliderMath.CalculatePosition(list, rate);
+                        break;
+                    case 1:
+                        value = SliderMath.SafeCalculateRotation(value, name, list, rate);
+                        break;
+                    default:
+                        value = SliderMath.CalculateScale(list, rate);
+                        break;
+                }
+            }
+        }
+        [ParameterByRef(new[] { 2 })]
+        [HarmonyPrefix, HarmonyPatch(typeof(AnimationKeyInfo), "GetInfo", new[] { typeof(string), typeof(float), typeof(Vector3[]), typeof(bool[]) })]
+        public static void GetInfoPrefix(ref float __state, ref float rate)
+        {
+            __state = rate;
+
+            if (rate > 1)
+                rate = 1f;
+
+
+            if (rate < 0)
+                rate = 0f;
+        }
+        [ParameterByRef(new[] { 2 })]
+        [HarmonyPostfix, HarmonyPatch(typeof(AnimationKeyInfo), "GetInfo", new[] { typeof(string), typeof(float), typeof(Vector3[]), typeof(bool[]) })]
+        public static void GetInfoPostfix(AnimationKeyInfo __instance, bool __result, float __state, string name, ref float rate, ref Vector3[] value, bool[] flag)
+        {
+            if (!__result)
+                return;
+
+            rate = __state;
+
+            if (rate < 0f || rate > 1f)
+            {
+                var dictInfo = (Dictionary<string, List<AnimationKeyInfo.AnmKeyInfo>>)akf_dictInfo.GetValue(__instance);
+
+                List<AnimationKeyInfo.AnmKeyInfo> list = dictInfo[name];
+
+
+                if (flag[0])
+                {
+                    value[0] = SliderMath.CalculatePosition(list, rate);
+                }
+
+                if (flag[1])
+                {
+                    value[1] = SliderMath.SafeCalculateRotation(value[1], name, list, rate);
+                }
+
+                if (flag[2])
+                {
+                    value[2] = SliderMath.CalculateScale(list, rate);
+                }
+            }
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(ChaFileControl), "CheckDataRange")]
+        public static bool CheckDataRangePreHook(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(ChaControl), nameof(ChaControl.Reload))]
+        public static void Reload(ChaControl __instance) => __instance.StartCoroutine(SliderUnlocker.ResetAllSliders());
+    }
+}
---
+++ EC.Core.SliderUnlocker/SliderUnlocker.cs
@@ -0,0 +1,290 @@
+﻿using BepInEx;
+using ChaCustom;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Harmony;
+using EC.Core.ConfigExtensions;
+using EC.Core.Internal;
+using BepInEx.Configuration;
+
+namespace EC.Core.SliderUnlocker
+{
+    [BepInPlugin(GUID, PluginName, Version)]
+    public class SliderUnlocker : BaseUnityPlugin
+    {
+        public const string GUID = "EC.Core.SliderUnlocker";
+        public const string PluginName = "Slider Unlocker";
+        public const string Version = Metadata.PluginsVersion;
+
+        /// <summary> Maximum value sliders can possibly extend </summary>
+        internal static float SliderAbsoluteMax => Math.Max(SliderMax, 5f);
+        /// <summary> Minimum value sliders can possibly extend </summary>
+        internal static float SliderAbsoluteMin => Math.Min(SliderMin, -5f);
+        /// <summary> Maximum value of sliders when not dynamically unlocked </summary>
+        internal static float SliderMax => (Maximum.Value < 100 ? 100 : Maximum.Value) / 100f;
+        /// <summary> Minimum value of sliders when not dynamically unlocked </summary>
+        internal static float SliderMin => (Minimum.Value > 0 ? 0 : Minimum.Value) / 100f;
+
+        private static readonly List<Target> _targets = new List<Target>();
+
+        #region Config
+        [AcceptableValueRange(-500, 0, false)]
+        public static ConfigWrapper<int> Minimum { get; private set; }
+        [AcceptableValueRange(100, 500, false)]
+        public static ConfigWrapper<int> Maximum { get; private set; }
+
+        public SliderUnlocker()
+        {
+            Minimum = Config.Wrap("Slider Config", "Minimum slider value", "Changes will take effect next time the editor is loaded or a character is loaded.", 0);
+            Maximum = Config.Wrap("Slider Config", "Maximum slider value", "Changes will take effect next time the editor is loaded or a character is loaded.", 100);
+        }
+        #endregion
+
+        protected void Awake()
+        {
+            Hooks.InstallHooks();
+            //VoicePitch.Init();
+
+            foreach (var type in typeof(CvsAccessory).Assembly.GetTypes())
+            {
+                if (type.Name.StartsWith("Cvs", StringComparison.OrdinalIgnoreCase) &&
+                    type != typeof(CvsDrawCtrl) &&
+                    type != typeof(CvsColor))
+                {
+                    var fields = type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+                    var inputFields = fields.Where(x => typeof(TMP_InputField).IsAssignableFrom(x.FieldType)).ToList();
+                    if (inputFields.Count == 0)
+                        continue;
+
+                    var sliders = fields.Where(x => typeof(Slider).IsAssignableFrom(x.FieldType)).ToList();
+                    if (sliders.Count == 0)
+                        continue;
+
+                    var buttons = fields.Where(x => typeof(Button).IsAssignableFrom(x.FieldType)).ToList();
+
+                    _targets.Add(new Target(type, inputFields, sliders, buttons));
+                }
+            }
+        }
+
+        private static void LevelFinishedLoading(Scene scene, LoadSceneMode mode) => SetAllSliders(scene);
+
+        /// <summary>
+        /// Sliders that don't work or have issues outside of the 0-100 limit
+        /// </summary>
+        private static readonly string[] SliderBlacklist = { "sldWaistLowW", "sldHairLength" };
+
+        internal static IEnumerator ResetAllSliders()
+        {
+            var sceneObjects = SceneManager.GetActiveScene().GetRootGameObjects();
+
+            //Set all sliders to maximum values so when the character is loaded they can be set correctly
+            foreach (var target in _targets)
+            {
+                var cvsInstances = sceneObjects.SelectMany(x => x.GetComponentsInChildren(target.Type, true));
+
+                foreach (var cvs in cvsInstances)
+                {
+                    if (cvs == null)
+                        continue;
+
+                    foreach (var x in target.Sliders)
+                    {
+                        var slider = (Slider)x.GetValue(cvs);
+                        if (slider != null)
+                        {
+                            slider.maxValue = SliderAbsoluteMax;
+                            slider.minValue = SliderAbsoluteMin;
+                        }
+                    }
+                }
+            }
+
+            //Wait for next frame so the character is loaded and values set
+            yield return null;
+            sceneObjects = SceneManager.GetActiveScene().GetRootGameObjects();
+
+            //Set all slider min/max back to the default unlocked state
+            foreach (var target in _targets)
+            {
+                var cvsInstances = sceneObjects.SelectMany(x => x.GetComponentsInChildren(target.Type, true));
+
+                foreach (var cvs in cvsInstances)
+                {
+                    if (cvs == null)
+                        continue;
+
+                    foreach (var x in target.Sliders)
+                    {
+                        var slider = (Slider)x.GetValue(cvs);
+                        if (slider != null)
+                        {
+                            UnlockSlider(slider, slider.value, SliderBlacklist.Contains(x.Name));
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void SetAllSliders(Scene scene)
+        {
+            var sceneObjects = scene.GetRootGameObjects();
+
+            foreach (var target in _targets)
+            {
+                var cvsInstances = sceneObjects.SelectMany(x => x.GetComponentsInChildren(target.Type, true));
+
+                foreach (var cvs in cvsInstances)
+                {
+                    if (cvs == null)
+                        continue;
+
+                    ResetAllRangesToDefault(target, cvs);
+
+                    var buttonClicked = false;
+                    foreach (var x in target.Fields)
+                    {
+                        var inputField = (TMP_InputField)x.GetValue(cvs);
+                        if (inputField == null)
+                            continue;
+
+                        inputField.characterLimit = 4;
+
+                        //Find the slider that matches this input field
+                        var sliderFieldInfo = target.Sliders.FirstOrDefault(y => y.Name.Substring(3) == x.Name.Substring(3));
+                        if (sliderFieldInfo == null)
+                            continue;
+
+                        var slider = (Slider)sliderFieldInfo.GetValue(cvs);
+                        if (slider == null)
+                            continue;
+
+                        //After reset button click reset the slider unlock state
+                        inputField.onValueChanged.AddListener(
+                            _ =>
+                            {
+                                if (buttonClicked)
+                                {
+                                    buttonClicked = false;
+                                    UnlockSliderFromInput(slider, inputField, SliderBlacklist.Contains(sliderFieldInfo.Name));
+                                }
+                            });
+
+                        //When the user types a value, unlock the sliders to accomodate
+                        inputField.onEndEdit.AddListener(_ => UnlockSliderFromInput(slider, inputField, SliderBlacklist.Contains(sliderFieldInfo.Name)));
+                    }
+
+                    foreach (var x in target.Buttons)
+                    {
+                        var button = (Button)x.GetValue(cvs);
+                        if (button == null)
+                            continue;
+
+                        //Find the slider that matches this button
+                        var sliderFieldInfo = target.Sliders.FirstOrDefault(y => y.Name.Substring(3) == x.Name.Substring(3));
+                        if (sliderFieldInfo == null)
+                            continue;
+
+                        var slider = (Slider)sliderFieldInfo.GetValue(cvs);
+                        if (slider == null)
+                            continue;
+
+                        //When the button is clicked set a flag used by InputFieldOnValueChanged
+                        button.onClick.AddListener(() => buttonClicked = true);
+                    }
+                }
+            }
+        }
+
+        private static void ResetAllRangesToDefault(Target target, UnityEngine.Component cvs)
+        {
+            foreach (var x in target.Sliders)
+            {
+                var slider = (Slider)x.GetValue(cvs);
+                if (slider != null)
+                {
+                    if (SliderBlacklist.Contains(x.Name))
+                        continue;
+
+                    //Set all sliders to the default unlock state
+                    slider.maxValue = SliderMax;
+                    slider.minValue = SliderMin;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Make sure the entered value is within range
+        /// </summary>
+        private static void UnlockSliderFromInput(Slider _slider, TMP_InputField _inputField, bool defaultRange)
+        {
+            var value = float.TryParse(_inputField.text, out var num) ? num / 100 : 0;
+
+            if (value > SliderAbsoluteMax)
+            {
+                _inputField.text = (SliderAbsoluteMax * 100).ToString();
+                value = SliderAbsoluteMax;
+            }
+            else if (value < SliderAbsoluteMin)
+            {
+                _inputField.text = (SliderAbsoluteMin * 100).ToString();
+                value = SliderAbsoluteMin;
+            }
+            UnlockSlider(_slider, value, defaultRange);
+        }
+
+        /// <summary>
+        /// Unlock or lock the slider depending on the entered value
+        /// </summary>
+        private static void UnlockSlider(Slider _slider, float value, bool defaultRange)
+        {
+            var valueRoundedUp = (int)Math.Ceiling(Math.Abs(value));
+            var max = defaultRange ? 1 : SliderMax;
+            var min = defaultRange ? 0 : SliderMin;
+
+            if (value > max)
+            {
+                _slider.minValue = min;
+                _slider.maxValue = valueRoundedUp;
+            }
+            else if (value < min)
+            {
+                _slider.minValue = -valueRoundedUp;
+                _slider.maxValue = max;
+            }
+            else
+            {
+                _slider.minValue = min;
+                _slider.maxValue = max;
+            }
+        }
+
+        #region MonoBehaviour
+        protected void OnEnable() => SceneManager.sceneLoaded += LevelFinishedLoading;
+        protected void OnDisable() => SceneManager.sceneLoaded -= LevelFinishedLoading;
+        #endregion
+
+        private sealed class Target
+        {
+            public Target(Type type, List<FieldInfo> fields, List<FieldInfo> sliders, List<FieldInfo> buttons)
+            {
+                Type = type;
+                Fields = fields;
+                Sliders = sliders;
+                Buttons = buttons;
+            }
+            public readonly Type Type;
+            public readonly List<FieldInfo> Fields;
+            public readonly List<FieldInfo> Sliders;
+            public readonly List<FieldInfo> Buttons;
+        }
+    }
+}
