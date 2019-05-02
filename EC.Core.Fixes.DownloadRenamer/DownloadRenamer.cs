ac4fea614e503f9328fdedb9cafd3b21de4e27af
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-05-02 00:21:53 -0500

    Rename project

--- EC.Core.Fixes.DownloadRenamer/DownloadRenamer.cs
+++ EC.Core.Fixes.DownloadRenamer/DownloadRenamer.cs
@@ -7,7 +7,7 @@ namespace EC.Core.DownloadRenamer
     public class DownloadRenamer : BaseUnityPlugin
     {
         public const string PluginName = "Download Renamer";
-        public const string GUID = "2f1827a3-7e2f-4a1d-8c46-24cf81f2d8d7";
+        public const string GUID = "EC.Core.Fixes.DownloadRenamer";
         public const string Version = Metadata.PluginsVersion;
 
         private void Start() => Hooks.InstallHooks();
