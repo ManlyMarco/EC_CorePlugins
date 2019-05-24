a68b0daf678c63fd0eb6abd3c89eb902354d40f6
ManlyMarco <dummyaccx@gmail.com>
2019-05-25 00:32:52 +0200

    Version up to 1.1

--- EC.Core/Internal/Metadata.cs
+++ EC.Core/Internal/Metadata.cs
@@ -6,7 +6,7 @@
         /// Version number used by all plugins. Must be convertible to <see cref="System.Version"/>
         /// Uses ReleaseVersion.HotfixVersion format.
         /// </summary>
-        public const string PluginsVersion = "1.0";
+        public const string PluginsVersion = "1.1";
         public const string CompanyName = "EC_CorePlugins";
     }
 }
